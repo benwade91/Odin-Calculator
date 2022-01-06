c0ef21721b612e4a6d604a0259322f62b26bd59b
benwade91 <70988204+benn925@users.noreply.github.com>
2022-01-05 16:00:14 -0800

    added calculator functionality

---
+++ script.js
@@ -0,0 +1,81 @@
+let outcome = 0;
+let inputString = '';
+let equation = [];
+const screen = document.querySelector('.screen');
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const negative = document.querySelector('#negative')
+const clear = document.querySelector('#clear')
+
+numbers.forEach(number => {
+    number.onclick = () => setInput(number.innerText);
+})
+
+negative.onclick = () => {
+    if (inputString == '') {
+        inputString = '-'
+    } else {
+        inputString = String(parseFloat(inputString) * -1)
+    }
+    screen.innerText = inputString;
+}
+
+clear.onclick = () => {
+    if (inputString == '') {
+        screen.innerText = 0;
+        equation = [];
+    } else {
+        inputString = '';
+        screen.innerText = '';
+        operators.forEach(x => {
+            x.style.backgroundColor = 'white';
+            x.style.color = 'black';
+        })
+    }
+}
+
+const setInput = (input) => {
+    inputString += input;
+    screen.innerText = inputString;
+
+    operators.forEach(operator => {
+        operator.onclick = () => setOperator(operator)
+    })
+}
+
+const setOperator = (operator) => {
+    equation.push(parseFloat(inputString))
+    equation.push(operator.innerText)
+    operator.style.backgroundColor = 'grey'
+    operator.style.color = 'white';
+    inputString = ''
+}
+
+const evaluate = () => {
+    inputString !== '' && equation.push(parseFloat(inputString))
+    if (equation.length == 3) {
+        switch (equation[1]) {
+            case '+':
+                screen.innerText = equation[0] + equation[2]
+                break;
+            case '-':
+                screen.innerText = equation[0] - equation[2]
+                break;
+            case 'x':
+                screen.innerText = equation[0] * equation[2]
+                break;
+            case '/':
+                screen.innerText = equation[0] / equation[2]
+                break;
+            default:
+                console.log('there was a problem');
+                break;
+        }
+        operators.forEach(x => {
+            x.style.backgroundColor = 'white';
+            x.style.color = 'black';
+        })
+        equation = [];
+    }
+}
+document.querySelector('#equal').onclick = () => evaluate();
